308858c0ec4c69be406904476572f1ca638401f1
gen <kyliki@yandex.ru>
2024-06-15 00:29:41 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/assets/styles'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+});

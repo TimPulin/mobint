59d671e2f37fc2a41e12e5ad7686ef364ee3f261
gen <kyliki@yandex.ru>
2024-06-16 17:16:28 +0300

    запросы корректны

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,41 @@
+import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
+
 import CardList from '@/components/card-list/CardList';
 import companiesStore from '@/stores/companies-store';
+import { getCards } from '@/api/server-connections';
+import MessageOnPage from '@/components/message-on-page/MessageOnPage';
 
-export default function MainPage() {
+const MainPage = observer(() => {
   const companyList = companiesStore.companies;
 
+  async function getCardsLocal() {
+    companiesStore.setIsLoading(true);
+
+    const response = await getCards(0, 5);
+    console.log(response);
+
+    companiesStore.setCompanies(response.companies);
+    companiesStore.setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getCardsLocal();
+  }, []);
+
   if (companyList.length === 0 && !companiesStore.isLoading) {
-    return <div>Нет компаний</div>;
+    return (
+      <main className="main">
+        <MessageOnPage text="Нет компаний" />
+      </main>
+    );
   }
 
   return (
-    <>
+    <main className="main">
       <CardList companyList={companyList} />
-    </>
+    </main>
   );
-}
+});
+
+export default MainPage;

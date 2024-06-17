da6ffe201e84f735e9121ff7d9afcaaea0ff4222
gen <kyliki@yandex.ru>
2024-06-17 23:36:08 +0300

    исправил стили для большого экрана

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@styles/basic-styles.css';
+import '@styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
